42c5f5ca65f608e01a795a40a7cbbaa0ee1c8bd9
Alex Serkin <zverev095@list.ru>
2024-07-30 17:10:29 +0300

    add homework1

---
+++ src/main/java/homework1/Runner.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+        Homework1.print();
+        System.out.println(Homework1.calculating(3,1,4,2));
+        System.out.println(Homework1.sumBool(9,20));
+        System.out.println(Homework1.isNegative(1));
+
+
+    }
+}
